5c91ce31679f42f9a664568236c00bd72b23d431
kjhop <kjhop@wp.pl>
2018-12-23 13:47:30 +0100

    Finished basic CRUD

--- backend/controllers/database.js
+++ backend/controllers/database.js
@@ -15,18 +15,35 @@ module.exports = app =>{
     app.use(bodyParser.urlencoded({
         extended:true
     }));
-
+    //Simple CRUD not really safe
     app.get("/", (request, response)=>{
         pool.query("SELECT * FROM ideas", (error, result, field)=>{
             if(error) throw error;
+            console.log("Listed all records");
             response.send(result);
         });
     });
     app.post('/', (request, response)=>{
         const sql = "INSERT INTO ideas(title, description, date) VALUES ('"+request.body.title+"','"+request.body.description+"','"+request.body.date+"')";
         pool.query(sql, (error, result)=>{
-            if (error) throw error
+            if (error) throw error;
             console.log("1 record inserted");
+            response.send();
+        });
+    });
+    app.delete("/", (request, response)=>{
+        const sql = "DELETE FROM ideas WHERE id=" + request.body.id;
+        pool.query(sql, (error, result)=>{
+            if(error) throw error;
+            console.log("1 record deleted");
+            response.send();
+        });
+    });
+    app.put("/", (request, response)=>{
+        const sql = "UPDATE ideas SET title='"+request.body.title+"', description='"+request.body.description+"', date='"+request.body.date+"' WHERE id="+request.body.id;
+        pool.query(sql, (error,result)=>{
+            if(error) throw error;
+            console.log("1 record updated");
             response.send();
         });
     });
